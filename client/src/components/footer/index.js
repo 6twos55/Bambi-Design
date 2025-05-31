294da7c10d4cb33f1f5472a3147f147a75b02be7
Sixtus Nwaogu <93261014+6twos55@users.noreply.github.com>
2025-05-31 12:57:57 +0100

    Update index.js

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <div className='Footer' style={{textAlign: "center", backgroundColor: "#22c2c5"}}>
+    <div className='Footer' style={{textAlign: "center", backgroundColor: "#22c2c5", padding: "10px 0"}}>
       Built with React and Material UI
     </div>
   )
